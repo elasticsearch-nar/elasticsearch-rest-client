091fba284495946d5a6d6779ef435f13a751272f
Deepak Nirala <dnirala1985@gmail.com>
2019-08-17 16:49:27 +0530

    Elastic search base

---
+++ src/main/java/com/elasticsearch/elasticsearchrestclientnar/ElasticsearchRestclientNarApplication.java
@@ -0,0 +1,13 @@
+package com.elasticsearch.elasticsearchrestclientnar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchRestclientNarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticsearchRestclientNarApplication.class, args);
+	}
+
+}
